16f52b427d10dc9c598036e339e028e879c662b4
Naanii13 <eriksen.nahir@gmail.com>
2018-11-09 17:48:02 -0300

    Creamos listado de producto
    -# Please enter the commit message for your changes. Lines starting

---
+++ src/AppBundle/Controller/TestController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+class TestController extends Controller
+{
+	/**
+* @Route("/test", name="AppBundle:TestController:indexAction")
+*/
+	public function indexAction()
+	{
+		return $this->render('test/index.html.twig' , 
+			[//'nombre'=>'juan', 
+			//'apellido' =>'gomez',
+			'Productos' =>  [[
+							'nombre'=>'televisor',
+							'precio'=>'19000'
+							],
+							[
+							'nombre'=>'cafetera',
+							'precio'=>'4500'
+							]
+							]])
+
+		;
+	}
+}
