f4738e30041dca31f8eab2f7f5e1d99404dbc034
Naanii13 <eriksen.nahir@gmail.com>
2018-11-12 17:55:14 -0300

    Commit inicial busqueda de productos

--- src/ProductoBundle/Controller/DefaultController.php
+++ src/ProductoBundle/Controller/DefaultController.php
@@ -4,14 +4,22 @@ namespace ProductoBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
-
+/*  */
 class DefaultController extends Controller
 {
     /**
      * @Route("/")
      */
+
     public function indexAction()
     {
+          
+    	$productos=$this->getDoctrine()
+ 		-> getRepository ('ProductoBundle:Producto')
+ 		-> findAll();
         return $this->render('ProductoBundle:Default:index.html.twig');
+
     }
+
+
 }
